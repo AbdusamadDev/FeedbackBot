14190700f39b0d60214fa337eb2ce4f6ba92e6da
AbdusamadDev <abdusamaddev571@gmail.com>
2023-12-01 16:37:14 +0500

    Update Database Design

--- api/models.py
+++ api/models.py
@@ -4,16 +4,8 @@ from django.core.validators import RegexValidator
 
 
 class Category(models.Model):
-    text = models.CharField(max_length=150)
-    admin = models.ForeignKey(to=Admin, on_delete=models.CASCADE)
-
-
-class Question(models.Model):
-    CHOICES = (("Yes", "Yes"), ("No", "No"))
-    admin = models.ForeignKey(to=Admin, on_delete=models.CASCADE)
-    category = models.ForeignKey(to=Category)
-    text = models.CharField(max_length=300)
-    status = models.BooleanField(choices=CHOICES)
+    title = models.CharField(max_length=150)
+    admin = models.ForeignKey(to=Admin, on_delete=models.CASCADE)  # Super Admin
     date_created = models.DateTimeField(auto_now_add=True)
 
 
@@ -27,12 +19,25 @@ class User(models.Model):
         """,
     )
 
-    phone_number = models.CharField(
-        validators=[phone_regex], max_length=17
-    )  # Adjust max_length as needed
+    phone_number = models.CharField(validators=[phone_regex], max_length=17)
+    region = models.CharField(max_length=50)
 
 
-class Feedback(models.Model):
+class Question(models.Model):
+    CHOICES = (("Yes", "Yes"), ("No", "No"))
+    text = models.CharField(max_length=300)
+    status = models.BooleanField(choices=CHOICES)
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+
+class FAQ(models.Model):
+    question = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
+    admin = models.ForeignKey(to=User, on_delete=models.CASCADE)  # Super Admin
+
+
+class Answer(models.Model):
+    text = models.TextField(max_length=250)
     question = models.ForeignKey(to=Question, on_delete=models.CASCADE)
-    description = models.TextField(max_length=400)
+    admin = models.ForeignKey(to=Admin, on_delete=models.CASCADE)  # Causal Admin
