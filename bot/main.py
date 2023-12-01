a8c8a0bf3ebaf1c989da8d6fcb14662c36a413a6
AbdusamadDev <abdusamaddev571@gmail.com>
2023-12-01 09:57:10 +0500

    Region Choice buttons

--- bot/main.py
+++ bot/main.py
@@ -1,15 +1,43 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.types import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    ReplyKeyboardRemove,
+)
 from aiogram.utils import executor
+from aiogram.utils.callback_data import CallbackData
 
-API_TOKEN = "YOUR_BOT_API_TOKEN"
+API_TOKEN = "6195275934:AAEngBypgfNw3SwcV9uV_jdatZtMvojF9cs"
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
 
 user_data = {}
+
+# Callback data factory
+region_callback = CallbackData("region", "name")
+
+# List of regions in Uzbekistan
+uzbekistan_regions = [
+    "Andijan",
+    "Bukhara",
+    "Fergana",
+    "Jizzakh",
+    "Khorezm",
+    "Namangan",
+    "Navoiy",
+    "Qashqadaryo",
+    "Samarkand",
+    "Sirdaryo",
+    "Surkhandarya",
+    "Tashkent",
+    "Tashkent City",
+    "Karakalpakstan",
+]
 
 
 @dp.message_handler(commands=["start"])
@@ -38,19 +66,29 @@ async def process_middle_name(message: types.Message):
 @dp.message_handler(lambda message: "phone_number" not in user_data)
 async def process_phone_number(message: types.Message):
     user_data["phone_number"] = message.text
-    await message.reply("Please enter your region:")
+
+    # Create InlineKeyboard for regions
+    inline_kb = InlineKeyboardMarkup(row_width=2)
+    for region in uzbekistan_regions:
+        inline_kb.add(
+            InlineKeyboardButton(region, callback_data=region_callback.new(name=region))
+        )
+
+    await message.reply("Please choose your region:", reply_markup=inline_kb)
 
 
-@dp.message_handler(lambda message: "region" not in user_data)
-async def process_region(message: types.Message):
-    user_data["region"] = message.text
+@dp.callback_query_handler(region_callback.filter())
+async def process_region_selection(query: types.CallbackQuery, callback_data: dict):
+    region = callback_data["name"]
+    user_data["region"] = region
 
     markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     markup.add(KeyboardButton("Most common questions"))
     markup.add(KeyboardButton("Disagree"))
     markup.add(KeyboardButton("Complain"))
 
-    await message.reply("Please choose an option:", reply_markup=markup)
+    await query.message.reply("Please choose an option:", reply_markup=markup)
+    await query.answer()
 
 
 @dp.message_handler(
