ef6e099f892ef9f157a56e473868b5602b608fbe
AbdusamadDev <abdusamaddev571@gmail.com>
2023-12-07 18:04:15 +0500

    Start in thread

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,17 +1,11 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
+import threading
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+def run_bot():
+    os.system("python3 ./bot/main.py")
 
 application = get_wsgi_application()
-os.system("python3 ./bot/main.py")
+threading.Thread(target=run_bot).start()
